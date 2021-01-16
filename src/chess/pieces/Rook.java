55e0929e76a9d625ba2abe0b29b5ff760827dfeb
Renato Adriano <renatobryant@gmail.com>
2021-01-16 18:44:10 -0300

    CheckMate logic

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -34,7 +34,7 @@ public class Rook extends ChessPiece {
 		
 		//left
 		p.setValues(position.getRow(), position.getColumn()-1);;
-		while (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
 			mat[p.getRow()][p.getColumn()]=true;
 			p.setColumn(p.getColumn()-1);
 		}
@@ -44,7 +44,7 @@ public class Rook extends ChessPiece {
 		
 		//right
 		p.setValues(position.getRow(), position.getColumn()+1);;
-		while (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
 			mat[p.getRow()][p.getColumn()]=true;
 			p.setColumn(p.getColumn()+1);
 		}
